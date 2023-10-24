5f0370f82e277eb3d4bd0a49fc9e04b75f39ce3d
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2023-10-24 17:16:07 +0530

    Updates

---
+++ precache-manifest.b33dd77b191c709eca2b2a71a373d8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a142df14232e99fa6cfc3f034b6e265",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "16dffdc5c11e9e52b7e3",
+    "url": "/Notes-App/static/css/main.7dd85925.chunk.css"
+  },
+  {
+    "revision": "c6d65312e2e72a3f4d37",
+    "url": "/Notes-App/static/js/2.cb3dadbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Notes-App/static/js/2.cb3dadbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16dffdc5c11e9e52b7e3",
+    "url": "/Notes-App/static/js/main.7af559b6.chunk.js"
+  },
+  {
+    "revision": "53ac33ff941e0a2d7e60",
+    "url": "/Notes-App/static/js/runtime-main.1e6c1761.js"
+  }
+]);
